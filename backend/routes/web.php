62cff1715d307869a4150f2cb968f291d8d97226
tokiya-takai <t.orca.dolphin@gmail.com>
2021-04-21 07:05:55 +0900

    Created common

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/index', [App\Http\Controllers\PostsController::class, 'index'])->name('index');
+Route::get('/', [App\Http\Controllers\PostsController::class, 'index'])->name('index');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
