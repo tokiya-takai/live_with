8625760d441462757a0cf9b19013e6a1a4ea43e1
tokiya-takai <t.orca.dolphin@gmail.com>
2021-04-21 08:58:06 +0900

    Create my page

--- backend/resources/views/posts/index.blade.php
+++ backend/resources/views/posts/index.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Index.blade.php</p>
+    <div id="my-list">
+        <div class="my-list-header">
+            <h1>MyShoes</h1>
+            <button class="filter">Filter</button>
+        </div>
+        <div class="shoes-list">
+            <ul>
+                <button>
+                    <li>
+                        <figure>
+                            <img src="images/test-image.jpeg" alt="shoe">
+                        </figure>
+                        <figcaption>
+                            <p class="shoe-name">チャーチ　バーウッド</p>
+                            <p class="last-maintenance">最後のメンテンナンス 2021/01/01</p>
+                        </figcaption>
+                    </li>
+                </button>
+            </ul>
+        </div>
+    </div>
 @endsection
