d69708b8935e8183a5f5c0ccf454ba09a218ec1e
tokiya-takai <t.orca.dolphin@gmail.com>
2021-04-21 16:52:41 +0900

    Created register page

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('You are logged in!') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,15 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="logged-message">
+                        <p>Thank you!</p>
+                        <p>Enjoy your shoe life.</p>
+                        <p>May all your dreams come true, I hope it goes well.</p>
+                        <p>I’m always on your side.</p>
+                        <p>We live with shoes.</p>
+                        <p><a href=" {{ route('index') }} "><button>Return to the home.</button></a></p>
+                        <figure><img src="{{ asset('images/livewith-sub-logo.png') }}" alt="logo"></figure>
+                    </div>
                 </div>
             </div>
         </div>
