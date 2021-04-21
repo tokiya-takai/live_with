6c73429d1584dcceb59d1223d90ade8773fb6bdc
tokiya-takai <t.orca.dolphin@gmail.com>
2021-04-21 17:18:58 +0900

    Header fix

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -39,20 +39,18 @@
                     @else
                         <li>
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
+                                {{ Auth::user()->name }}</a>
+                        </li>
+                        <li>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                                    {{ __('Logout') }}</a>
                             </div>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </li>
                     @endguest
                 </ul>
