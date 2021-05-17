04894a3108849429024e20fba6b8cf6a03d08388
tokiya-takai <t.orca.dolphin@gmail.com>
2021-05-17 18:36:34 +0900

    DefaultStringLength 191 -> 1000

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Schema::defaultStringLength(191);
+        \Schema::defaultStringLength(1000);
 
         if (\App::environment(['heroku'])) {
             \URL::forceScheme('https');
