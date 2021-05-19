41dd9e12b35f6e3a24d2e854323e15606a8a61b9
tokiya-takai <t.orca.dolphin@gmail.com>
2021-05-19 13:15:38 +0900

    For others, I hid the edit button.

--- backend/resources/views/posts/show.blade.php
+++ backend/resources/views/posts/show.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
   <div id="show-shoe">
-    <a href=" {{ route('posts.edit', ['id'=>$item->id]) }}" class="new-post-btn"><button>Edit</button></a>
+    @if($item->id == Auth::id())
+      <a href=" {{ route('posts.edit', ['id'=>$item->id]) }}" class="new-post-btn"><button>Edit</button></a>
+    @endif
     <div class="show-shoe-header">
       <h1>{{ $item->title }}</h1>
     </div>
