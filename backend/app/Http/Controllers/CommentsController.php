433f491476b1e2aad1436848303a287c8639c04c
tokiya-takai <t.orca.dolphin@gmail.com>
2021-05-24 14:12:26 +0900

    Create comments controller

---
+++ backend/app/Http/Controllers/CommentsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    public function create() {
+
+    }
+
+    public function delete() {
+        
+    }
+}
